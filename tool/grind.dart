cfda4026e311be36aaa7cbe5727af59f9ae584cb
zoechi <gzoechi@gmail.com>
2015-06-08 09:21:47 +0200

    - disable test in Grinder tasks because this package doesn't have tests.

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,3 +1,12 @@
 library http_exception.tool.grind;
 
-export 'package:bwu_utils_dev/grinder/default_tasks.dart';
+export 'package:bwu_utils_dev/grinder/default_tasks.dart' hide main;
+import 'package:bwu_utils_dev/grinder/default_tasks.dart'
+    show grind, coverageTask, testTask;
+
+main(List<String> args) {
+  // this package doesn't have tests
+  testTask = ([_]) {};
+  coverageTask = () {};
+  grind(args);
+}
