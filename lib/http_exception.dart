20bd8baccbe3aaa7c540ded7169ac3ff7f38e150
Andres Garcia (TECH-ANDGAR) <dev@tech-andgar.me>
2024-02-26 00:28:32 -0500

    feat(extension): Add int HTTP status extension
    Introduced a new extension on the integer type to streamline the creation of HttpException instances based on HTTP status codes. This includes optional parameters for additional details, data payload, and URI.

--- lib/http_exception.dart
+++ lib/http_exception.dart
@@ -1,7 +1,7 @@
 /// A comprehensive and customizable system for handling HTTP exception in Dart applications.
 library http_exception;
 
+export 'src/extensions/extensions.dart';
 export 'src/http_4xx_exceptions.dart';
 export 'src/http_5xx_exceptions.dart';
 export 'src/http_exception_base.dart';
-export 'src/http_status_extension.dart';
